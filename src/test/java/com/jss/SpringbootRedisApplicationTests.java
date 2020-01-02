b535f780b3096635ee80f10e9642d6664d2a8cdd
jss <2921085905@qq.com>
2020-01-02 21:48:41 +0800

    springboot-redis

---
+++ src/test/java/com/jss/SpringbootRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jss;
+
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRedisApplicationTests {
+
+    void contextLoads() {
+    }
+
+}
